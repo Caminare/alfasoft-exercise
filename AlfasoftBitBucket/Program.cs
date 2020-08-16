f92620511e60ddda648e2147d9944f487f595ef4
Gabriel Vitor <gxvitor.1997@hotmail.com>
2020-08-16 11:58:39 -0300

    #Adding function to write requests do log file

--- AlfasoftBitBucket/Program.cs
+++ AlfasoftBitBucket/Program.cs
@@ -86,21 +86,34 @@ namespace AlfasoftBitBucket
             using (HttpClient client = new HttpClient())
             {
                 Console.WriteLine("User {0}", user);
+                Console.WriteLine("Requesting {0}", baseUrl);
 
                 HttpResponseMessage response = await client.GetAsync(baseUrl);
 
-                if (response.IsSuccessStatusCode)
-                {
-                    string data = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine(data);
-                }
-
-                else if(response.StatusCode == HttpStatusCode.NotFound)
-                    Console.WriteLine("User not found");
+                string data = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response: {0}", data);
 
                 Console.WriteLine("\n -------------------------------------------------------------------------------------------- \n");
 
+                LogRequestToFile(user, baseUrl, data);
+
             }
+        }
+
+
+        private static void LogRequestToFile(string user, string request, string data)
+        {
+            string currentPath = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(currentPath, "requests-log.txt");
+
+            using(StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine("User {0}", user);
+                writer.WriteLine("Request: {0}", request);
+                writer.WriteLine("Response: {0}", data);
+                writer.WriteLine("-------------------------------------------------------------------------------------");
+            }
+
         }
     }
 }
