672444ddce7ebd42e6901d2cd535cf008ff3753e
Gabriel Vitor <gxvitor.1997@hotmail.com>
2020-08-16 10:39:14 -0300

    #Adding path validation/function to close in 5sec

--- AlfasoftBitBucket/Program.cs
+++ AlfasoftBitBucket/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 
 namespace AlfasoftBitBucket
 {
@@ -6,7 +9,63 @@ namespace AlfasoftBitBucket
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = string.Empty;
+            bool validPath = false;
+
+            while (!validPath)
+            {
+                Console.WriteLine("Please, insert a valid path to the users file:");
+                path = Console.ReadLine();
+
+                validPath = IsPathValid(path);
+
+                if (!validPath)
+                    Console.WriteLine("The path is invalid");
+                else
+                    Console.WriteLine("Starting to process the file...");
+            }
+
+
+            string[] usersFile = ReturnUsers(path);
+
+            if (usersFile.Length == 0)
+            {
+                Console.WriteLine("No users were found in the file!");
+            }
+
+
+            foreach (string user in usersFile)
+            {
+                Console.WriteLine(user);
+            }
+
+            CloseApp();
+
+        }
+
+        private static bool IsPathValid(string path)
+        {
+            //Checking if the file is a txt
+            if (!path.Contains(".txt"))
+                return false;
+            //Else, check if the path exists
+            return File.Exists(path);
+        }
+
+        private static string[] ReturnUsers(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+
+        private static void CloseApp()
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                Console.WriteLine("Closing the app in {0} seconds...", i);
+                Thread.Sleep(1000);
+            }
+
+            Environment.Exit(0);
         }
     }
 }
